52ebda1835764d7ca2e43d59327886455ff5f1bb
aliceduchemin <aduchemi@student.42.fr>
2020-07-15 20:23:05 +0200

    notes parser

--- srcs/parser.c
+++ srcs/parser.c
@@ -12,9 +12,18 @@ t_command ft_parser(char *line)
     while(line[i] != ' ' && line[i])
         i++;
     c.k_end = i;
+
     c.a_begin = i + 1;
     while(line[i] != '\0')
-        i++;
+    {
+    	//realloc char si diff " ' > space et pas debut d'un redic en debut mot
+	// si space realloc une fois (cmp avec malloc en cours arg)
+	// si " ', cherche second, realloc ou erreur
+	// si redir, chg struct
+	
+	i++;
+	}
+	// supprimer space a la fin de arg
     c.a_end = i;
 
  //   printf("%d %d %d %d\n", c.k_begin, c.k_end, c.a_begin, c.a_end);
