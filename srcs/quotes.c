658ec2c328a07605818c28a1b9d3e419fe0a57d9
albankerloch <54630416+albankerloch@users.noreply.github.com>
2020-07-27 12:15:58 +0200

    Update quotes.c

--- srcs/quotes.c
+++ srcs/quotes.c
@@ -9,37 +9,6 @@ int    ft_backslash(t_command *c, char *line, int *i)
     return (0);
 }
 
-<<<<<<< HEAD
-int    ft_check_end_quote(t_command *c, char *line, int i)
-{
-    if (line[i] == '\"' && c->quote != 2)
-    {
-        i++;
-        while (line[i] != '\0')
-        {
-            if (line[i] == '\"' && line[i - 1] && line[i - 1] != '\\')
-                return (0);
-            i++;
-        }
-        c->arg = NULL;
-        printf("erreur double quote ouverte i=%d\n", i);
-        return (1);
-    }
-    else if (line[i] == '\'' && c->quote != 1)
-    {
-        i++;
-        while (line[i] != '\0')
-        {
-            if (line[i] == '\'' && line[i - 1] && line[i - 1] != '\\')
-                return (0);
-            i++;
-        }
-        c->arg = NULL;
-        printf("erreur simple quote ouverte i=%d\n", i);
-        return (1);
-    }
-    return (0);
-=======
 void    ft_skip_quotes(t_command *c, char *line, int *i)
 {
     while ((line[*i] == '\'' && c->quote != 2) || (line[*i] == '\"' && c->quote != 1))
@@ -52,5 +21,4 @@ void    ft_skip_quotes(t_command *c, char *line, int *i)
             c->quote = 0;
         (*i)++;
     }
->>>>>>> origin/quotes_v_alban
 }
