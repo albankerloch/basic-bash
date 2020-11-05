1472fe0868b1bd15320a74121f437f2ed5881bd6
aliceduchemin <aduchemi@student.42.fr>
2020-11-05 21:35:01 +0100

    fix détails quotes et caractères échappement

--- srcs/quotes.c
+++ srcs/quotes.c
@@ -34,7 +34,7 @@ int    ft_backslash(char *line, int *i, int *quote)
     }
     if (line[*i] && line[*i] == '\\' && *quote == 0)
         (*i)++;
-    else if (line[*i] && line[*i] == '\\' && *quote == 2 && line[*i + 1] && (line[*i + 1] == '`' || line[*i + 1] == '\"'))
+    else if (line[*i] && line[*i] == '\\' && *quote == 2 && line[*i + 1] && (line[*i + 1] == '`' || line[*i + 1] == '\"' || line[*i + 1] == '\\'))
         (*i)++;
     return (0);
 }
