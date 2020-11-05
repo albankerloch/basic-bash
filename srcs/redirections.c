de242b99441f5f0f014b606b8550316a7970b468
aliceduchemin <aduchemi@student.42.fr>
2020-11-06 00:23:28 +0100

    norminette en cours parser.c redirections.c

--- srcs/redirections.c
+++ srcs/redirections.c
@@ -12,27 +12,44 @@
 
 #include "../includes/minishell.h"
 
-int    ft_redirection_right(t_command *c, char *line, int *i, t_fix *fix)
+int ft_redirections(t_list *t, char *line, int *i)
+{
+    int ret;
+    
+    if (line[*i] == '>')
+    {
+        if ((ret = ft_redirection_right(t->content, line, i)) != 1)
+            return (ret);
+    }
+    else if (line[*i] == '<')
+    {
+        if ((ret = ft_redirection_left(t->content, line, i)) != 1)
+            return (ret);
+    }
+    return (1);
+}
+
+int    ft_redirection_right(t_command *c, char *line, int *i)
 {
     if (line[*i + 1] && line[*i + 1] == '>')
     {
         *i = *i + 2;
         c->add = 2;
-        return (ft_redir_right(c, line, i, fix));
+        return (ft_redir_right(c, line, i));
     }
     *i = *i + 1;
     c->add = 1;
-    return (ft_redir_right(c, line, i, fix));
+    return (ft_redir_right(c, line, i));
 }
 
-int    ft_redir_right(t_command *c, char *line, int *i, t_fix *fix)
+int    ft_redir_right(t_command *c, char *line, int *i)
 {
     int ret;
 
     while (line[*i] == ' ')
         (*i)++;
     if (!line[*i])
-        return (ft_syntax_error(fix, "newline"));
+        return (ft_syntax_error(&fix, "newline"));
     if (c->n_out[0] != '\0')
     {
         free(c->n_out);
@@ -40,12 +57,12 @@ int    ft_redir_right(t_command *c, char *line, int *i, t_fix *fix)
             return (0);
         c->n_out[0] = '\0';
     }
-    if ((ret = ft_new_out(c, line, i, fix)) != 1)
+    if ((ret = ft_new_out(c, line, i, &fix)) != 1)
         return (ret);
     return (ft_touch(c));
 }
 
-int    ft_redirection_left(t_command *c, char *line, int *i, t_fix *fix)
+int    ft_redirection_left(t_command *c, char *line, int *i)
 {
     int ret;
 
@@ -54,7 +71,7 @@ int    ft_redirection_left(t_command *c, char *line, int *i, t_fix *fix)
     while (line[*i] == ' ')
         *i = *i + 1;
     if (!line[*i])
-        return (ft_syntax_error(fix, "newline"));
+        return (ft_syntax_error(&fix, "newline"));
     if (c->n_input[0] != '\0')
     {
         free(c->n_input);
@@ -62,9 +79,9 @@ int    ft_redirection_left(t_command *c, char *line, int *i, t_fix *fix)
             return (0);
         c->n_input[0] = '\0';
     }
-    if ((ret = ft_new_input(c, line, i, fix)) != 1)
+    if ((ret = ft_new_input(c, line, i, &fix)) != 1)
         return (ret);
-    return (ft_checkfile(c, fix));
+    return (ft_checkfile(c, &fix));
 }
 
 int   ft_ambiguous_redir(char *line, int i_start, int *i, t_fix *fix)
