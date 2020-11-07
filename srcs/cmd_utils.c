b1bad15d796283532e8b1cdd1adab25356b957c7
Alban <alban.kerloch@gmail.com>
2020-11-07 15:23:47 +0100

    cmd utils norme OK

--- srcs/cmd_utils.c
+++ srcs/cmd_utils.c
@@ -1,6 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cmd_utils.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aduchemi <aduchemi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/15 16:24:18 by aduchemi          #+#    #+#             */
+/*   Updated: 2020/07/15 16:59:40 by aduchemi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../includes/minishell.h"
 
-void ft_strjoin_buff(char s1[PATH_MAX], char s2[PATH_MAX])
+void	ft_strjoin_buff(char s1[PATH_MAX], char s2[PATH_MAX])
 {
 	int		i;
 	int		j;
@@ -20,7 +32,7 @@ void ft_strjoin_buff(char s1[PATH_MAX], char s2[PATH_MAX])
 	s1[i] = 0;
 }
 
-void ft_realloc_concat_buff(char line[PATH_MAX], char c)
+void	ft_realloc_concat_buff(char line[PATH_MAX], char c)
 {
 	line[ft_strlen(line)] = c;
 	line[ft_strlen(line) + 1] = '\0';
